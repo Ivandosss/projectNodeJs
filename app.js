842b42e8fc70ba4f76b3d4ac68b3ea5c7b1039f9
Pop-Os <ivan@pop-os.localdomain>
2022-03-21 01:55:50 -0300

    :wrench: Configuração do Banco
    Banco configurado e tabela criada

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
 const express = require('express');
+const db = require('./db/connection');
 
 const app = express();
 const PORT = 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'));
 app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
+
+
+// db connection
+db
+  .authenticate()
+  .then(() => {
+    console.log("Conectou ao abnco com sucesso");
+  })
+  .catch(err => {
+    console.log("Ocorreu um erro ao conectar");
+  });
+
+
+// routes
+app.get('/', (req, res) => res.send('Hello World!'));
