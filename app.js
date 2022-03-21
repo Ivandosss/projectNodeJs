73b33d9e4c785450310ba84d81f1f68d343d895b
Pop-Os <ivan@pop-os.localdomain>
2022-03-21 02:45:57 -0300

    :memo: criando Models e rotas para adicionar registros
    criando Models e rotas para adicionar registros

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const db = require('./db/connection');
+const bodyParser = require('body-parser');
 
 const app = express();
 const PORT = 3000;
 
 app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
 
+// body parser
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // db connection
 db
@@ -17,6 +20,9 @@ db
     console.log("Ocorreu um erro ao conectar");
   });
 
+
+// routes jobs
+app.use('/jobs', require('./routes/jobs'));
 
 // routes
 app.get('/', (req, res) => res.send('Hello World!'));
